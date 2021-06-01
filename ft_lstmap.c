c3fc2fd6d6ae3c5fb0d6e904db1cab74749e5f43
Freya-Tenebrae <34307281+Freya-Tenebrae@users.noreply.github.com>
2021-06-01 11:32:46 +0200

    update lstmap

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -17,19 +17,21 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	t_list	*newlst;
 	t_list	*newelem;
 
-	(void)del;
-	if (lst == 0 || f == 0 || del == 0)
-		return (0);
+	if (lst == NULL || f == NULL)
+		return (NULL);
 	newelem = ft_lstnew(f(lst->content));
 	if (!newelem)
-		return (0);
+		return (NULL);
 	newlst = newelem;
 	lst = lst->next;
-	while (lst != 0)
+	while (lst != NULL)
 	{
 		newelem = ft_lstnew(f(lst->content));
 		if (!newelem)
-			return (0);
+        {
+            ft_lstclear(&newlst, del);
+			return (NULL);
+        }
 		ft_lstadd_back(&newlst, newelem);
 		lst = lst->next;
 	}
